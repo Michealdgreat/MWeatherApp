2a4cb9d7742b5cefc8b706b24d2ef6741e5fc640
Micheal Shodamola <michealshodamola01@gmail.com>
2024-12-10 01:22:43 +0200

    Add project files.

---
+++ MWeatherApp/MVVM/Model/LocationModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MWeatherApp.MVVM.Model
+{
+    public class LocationModel
+    {
+        public int Version { get; set; }
+        public string? Key { get; set; }
+        public string? Type { get; set; }
+        public int? Rank { get; set; }
+        public string? LocalizedName { get; set; }
+        public string? EnglishName { get; set; }
+        public Region? Region { get; set; }
+        public Country? Country { get; set; }
+
+        public GeoPosition? GeoPosition { get; set; }
+
+        public ParentCity? ParentCity { get; set; }
+
+    }
+
+}
---
+++ MWeatherApp/MVVM/Model/ParentCity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MWeatherApp.MVVM.Model
+{
+    public class ParentCity
+    {
+        public string? Key { get; set; }
+        public string? LocalizedName { get; set; }
+        public string? EnglishName { get; set; }
+    }
+}
