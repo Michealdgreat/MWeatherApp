214bc81d3c30915a59aba6838bed17f03156efb5
Micheal Shodamola <michealshodamola01@gmail.com>
2024-12-16 04:35:34 +0200

    Add Onboarding page.

---
+++ MWeatherApp/MVVM/ViewModels/OnboardingViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MWeatherApp.MVVM.ViewModels.Base;
+using MWeatherApp.MVVM.Views;
+using System.ComponentModel;
+
+namespace MWeatherApp.MVVM.ViewModels
+{
+    public partial class OnboardingViewModel : BaseViewModel
+    {
+
+
+        [RelayCommand]
+        private async Task GetStarted()
+        {
+            await Shell.Current.GoToAsync(nameof(WelcomePage), true);
+        }
+    }
+}
---
+++ MWeatherApp/MVVM/Views/OnboardingPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using MWeatherApp.MVVM.ViewModels;
+
+namespace MWeatherApp.MVVM.Views
+{
+    public partial class OnboardingPage : ContentPage
+    {
+        public OnboardingPage(OnboardingViewModel onboardingViewModel)
+        {
+            InitializeComponent();
+            BindingContext = onboardingViewModel;
+        }
+    }
+}
